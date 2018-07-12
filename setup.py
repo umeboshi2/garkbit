9f0b7d794aee40baf940149ac376adce27284d21
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-11 19:11:04 -0500

    update reqs

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     include_package_data=True,
     install_requires=requirements,
     dependency_links=[
-        'https://github.com/umeboshi2/hubby/archive/master.tar.gz#egg=hubby'
+        'https://github.com/umeboshi2/hubby/archive/repackage.tar.gz#egg=hubby'
     ],
     license="UNLICENSED",
     zip_safe=False,
