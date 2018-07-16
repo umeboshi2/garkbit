583c9e75e6fc29f6fb52dec174d31e81814b26bc
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-16 11:31:31 -0500

    more work on proxy view

--- garkbit/views/proxy.py
+++ garkbit/views/proxy.py
@@ -23,11 +23,25 @@ last_modified_format = "%a, %d %b %Y %H:%M:%S %Z"
 class ProxyView(BaseUserViewCallable):
     def __init__(self, request):
         super(ProxyView, self).__init__(request)
-        print(request.subpath)
         protocol = request.subpath[0]
         hostpath = os.path.join(*request.subpath[1:])
         url = "{}//{}".format(protocol, hostpath)
         if request.query_string:
             url = "{}?{}".format(url, request.query_string)
-        req = requests.get(url, stream=True)
-        self.response = Response(body_file=req.raw)
+        rheaders = dict([(k, v) for (k, v) in request.headers.items()
+                         if k != 'Host'])
+        resp = requests.request(
+            method=request.method,
+            url=url,
+            headers=rheaders,
+            cookies=request.cookies,
+            allow_redirects=False,
+            stream=True)
+        excluded_headers = ['content-encoding', 'content-length',
+                            'transfer-encoding', 'connection']
+        excluded_headers = []
+        headers = [(k, v) for (k, v) in resp.raw.headers.items()
+                   if k.lower() not in excluded_headers]
+        response = Response(body_file=resp.raw, headers=dict(headers),
+                            status_code=resp.status_code)
+        self.response = response
