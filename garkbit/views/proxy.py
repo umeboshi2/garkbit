d0f6628439eda9d55b57501481ef06a06bb846bd
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-08-10 13:06:28 -0500

    update proxy view to use wsgiproxy and base64 encoded url requests

--- garkbit/views/proxy.py
+++ garkbit/views/proxy.py
@@ -1,15 +1,11 @@
 import os
-# from configparser import ConfigParser
-# from datetime import datetime
-from urllib.error import HTTPError
-from pyramid.response import FileResponse, Response
-
-from pyramid.view import view_config, view_defaults
+import base64
+from urllib.parse import urlparse, urlunparse
 
 from trumpet.views.base import BaseUserViewCallable
-import requests
-from io import BytesIO
+from wsgiproxy import HostProxy
 
+from webob import Request
 
 APIROOT = '/api/dev/bsapi'
 
@@ -23,25 +19,15 @@ last_modified_format = "%a, %d %b %Y %H:%M:%S %Z"
 class ProxyView(BaseUserViewCallable):
     def __init__(self, request):
         super(ProxyView, self).__init__(request)
-        protocol = request.subpath[0]
-        hostpath = os.path.join(*request.subpath[1:])
-        url = "{}//{}".format(protocol, hostpath)
-        if request.query_string:
-            url = "{}?{}".format(url, request.query_string)
-        rheaders = dict([(k, v) for (k, v) in request.headers.items()
-                         if k != 'Host'])
-        resp = requests.request(
-            method=request.method,
-            url=url,
-            headers=rheaders,
-            cookies=request.cookies,
-            allow_redirects=False,
-            stream=True)
-        excluded_headers = ['content-encoding', 'content-length',
-                            'transfer-encoding', 'connection']
-        excluded_headers = []
-        headers = [(k, v) for (k, v) in resp.raw.headers.items()
-                   if k.lower() not in excluded_headers]
-        response = Response(body_file=resp.raw, headers=dict(headers),
-                            status_code=resp.status_code)
-        self.response = response
+        encoded = request.matchdict.get('encoded')
+
+        # FIXME  is there a better way rather than encode/decode?
+        url = base64.decodebytes(encoded.encode()).decode()
+        parsed = urlparse(url)
+
+        # FIXME this is ugly
+        app_url = urlunparse(list(parsed[:2]) + ['', '', '', ''])
+
+        proxy = HostProxy(app_url, client='requests')
+        preq = Request.blank(url)
+        self.response = preq.get_response(proxy)
