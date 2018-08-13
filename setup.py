9ffa8f0a07640b9f53f40993e6ab1bde5a0f0da7
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-08-13 18:32:37 -0500

    update hattie

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'hornstone>=0.1.8',
     'trumpet>=0.2.6',
-    'hattie>=0.2.19',
+    'hattie>=0.2.20',
     'gunicorn',
     'Akhet',
     'alembic',
