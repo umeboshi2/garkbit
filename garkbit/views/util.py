b5fd8d1a0dc3c096fbf208f01cb47da328724902
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-06-05 12:52:59 -0500

    cleanup views

--- garkbit/views/util.py
+++ garkbit/views/util.py
@@ -2,27 +2,18 @@ import os
 
 from pyramid.renderers import render
 
+def curly_brace_param(param):
+    return '{{{}}}'.format(param)
+
+
 # for cornice
 # @resource(**make_resource(path, ident='name'))
 def make_resource(rpath, ident='id', cross_site=True):
-    path = os.path.join(rpath, '{%s}' % ident)
+    path = os.path.join(rpath, curly_brace_param(ident))
     data = dict(collection_path=rpath, path=path)
     if cross_site:
         data['cors_origins'] = ('*',)
     return data
-
-
-def make_app_page(appname, settings, basecolor=None, request=None):
-    template = 'garkbit:templates/mainview.mako'
-    if basecolor is None:
-        basecolor = settings.get('default.css.basecolor', 'vanilla')
-    csspath = settings.get('default.css.path', '/assets/stylesheets')
-    jspath = settings.get('default.js.path', '/assets/client')
-    env = dict(appname=appname,
-               basecolor=basecolor,
-               csspath=csspath,
-               jspath=jspath)
-    return render(template, env, request=request)
 
 
 def apply_pagination(request, query):
