336305bc1d178411efcedc6cb7e3cee1cc2f9fd7
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-06-29 15:46:23 -0500

    user.id is uuid

--- garkbit/util.py
+++ garkbit/util.py
@@ -1,13 +1,13 @@
 from .models.usergroup import User
 
+
 def make_token(request, user):
     claims = dict(name=user.username, fullname=user.fullname,
-                  email=user.email, uid=user.id,
+                  email=user.email, uid=str(user.id),
                   groups=user.get_groups())
-    return request.create_jwt_token(user.id, **claims)
+    return request.create_jwt_token(str(user.id), **claims)
 
 
 def get_user(request):
     userid = request.unauthenticated_userid
     return request.dbsession.query(User).get(userid)
-    
