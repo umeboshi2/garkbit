47be3721b0d8d606188e196f8d1dbc6f7d914c8b
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-07 17:05:13 -0500

    update groupfinder to use name

--- garkbit/__init__.py
+++ garkbit/__init__.py
@@ -21,7 +21,7 @@ def groupfinder(userid, request):
     :return:
     """
     if userid and hasattr(request, 'user') and request.user:
-        groups = ['group:%s' % g.id for g in request.user.groups]
+        groups = ['group:%s' % g.name for g in request.user.groups]
         return groups
     return []
 
