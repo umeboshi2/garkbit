d144e5d772f096cd1825ee1f1b8f80c381b71225
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-10 14:45:46 -0500

    work on dbadmin

--- garkbit/views/dbadmin.py
+++ garkbit/views/dbadmin.py
@@ -1,66 +1,125 @@
 import os
 import io
+import base64
+import hashlib
 import lzma
 import json
 
-# from pyramid.view import view_config
+
+from pyramid.view import view_config
+from pyramid.security import Allow
+from pyramid.httpexceptions import HTTPNotFound
 from cornice.resource import resource
 import transaction
 
+from hornstone.alchemy import export_models
 # from trumpet.views.resourceviews import BaseResource
 from trumpet.views.resourceviews import apiroot
 from trumpet.views.resourceviews import BaseModelResource
 from trumpet.views.base import BaseUserViewCallable
 
+
+from ..models.usergroup import User, Group, UserGroup
+from ..models.geoposition import GeoPosition
 from ..models.site_document import SiteDocument
 from ..models.wikipage import WikiPage
 from ..models.todo import Todo
+from ..models.hourly import Worker, WorkSession
+# from ..models.object_summary import ObjectSummary
+
 
 from .util import make_resource
 
 
-APIROOT = os.path.join(apiroot(), 'dbadmin', '{model}')
+ALL_MODELS = dict(user=User, group=Group, usergroup=UserGroup,
+                  geoposition=GeoPosition, site_document=SiteDocument,
+                  wikipage=WikiPage, todo=Todo, worker=Worker,
+                  worksession=WorkSession)
+
+USERGROUP_MODELS = [
+    User,
+    Group,
+    UserGroup,
+]
+
+ALL_MODELS = [
+    GeoPosition,
+    SiteDocument,
+    WikiPage,
+    Todo,
+    Worker,
+    WorkSession,
+    ]
 
 
-def export_models(session, models):
-    data = dict()
-    output = io.BytesIO()
-    with transaction.manager:
-        with lzma.LZMAFile(output, 'w') as zfile:
-            q = session.query(model)
-            name = model.__name__
-            print("Dumping {}".format(name))
-            mlist = [m.serialize() for m in q]
-            data[name] = mlist
-            print("Exported {}".format(name))
-            content = json.dumps(data).encode()
-            zfile.write(content)
-    del data
-    return output.getvalue()
+def create_model(Model, obj):
+    model = Model()
+    for key in obj.keys():
+        setattr(model, key, obj[key])
+    return model
 
 
-@resource(**make_resource(APIROOT))
-class DbAdminResource(BaseModelResource):
-    def __init__(self, request, context=None):
-        super(DbAdminResource, self).__init__(request, context=context)
-        self.limit = None
-        self.max_limit = None
-        self._use_pagination = False
 
-    @property
-    def model(self):
-        return self.model_map.get(self.request.matchdict['model'])
+@resource(collection_path="/api/dev/dbadmin",
+          path="/api/dev/dbadmin/{view}",
+          permission='dbadmin')
+class DbAdminView(BaseModelResource):
+    def __acl__(self):
+        acl = [
+            (Allow, 'group:admin', 'dbadmin'),
+            ]
+        return acl
 
-    @property
-    def model_map(self):
-        return dict(wikipage=WikiPage, todo=Todo,
-                    sitedoc=SiteDocument)
+    def get(self):
+        view = self.request.matchdict['view']
+        if view == 'export-models':
+            return self.export_database()
+        elif view == 'delete-models':
+            return self.delete_all()
+        else:
+            raise HTTPNotFound
 
-    def collection_get(self):
-        print("DB", self.db)
-        print("MODEL", self.model)
-        return super(DbAdminResource, self).collection_get()
+    def post(self):
+        v = self.request.matchdict['view']
+        if v == 'import-models':
+            return self.import_data()
+        else:
+            raise HTTPNotFound
 
+    def export_database(self):
+        session = self.request.dbsession
+        models = USERGROUP_MODELS + ALL_MODELS
+        content = export_models(session, models)
+        s = hashlib.sha256()
+        s.update(content)
+        return dict(content=base64.encodebytes(content),
+                    sha256sum=s.hexdigest())
 
-class WikipageDbadmin(BaseUserViewCallable):
-    pass
+    def import_data(self):
+        data = self.import_zipfile()
+        with transaction.manager:
+            for Model in ALL_MODELS:
+                name = Model.__name__
+                objects = data[name]
+                print("{} has {} objects.".format(name, len(objects)))
+                for obj in objects:
+                    model = create_model(Model, obj)
+                    self.request.dbsession.add(model)
+                print("Imported {}".format(name))
+        return data
+
+    def delete_all(self):
+        with transaction.manager:
+            for Model in reversed(ALL_MODELS):
+                query = self.request.dbsession.query(Model)
+                query.delete()
+        return dict(result='success')
+
+    def import_zipfile(self):
+        text = self.request.json['content']
+        content = base64.decodestring(text.encode())
+        bfile = io.BytesIO(content)
+        zfile = lzma.open(bfile, 'rt')
+        data = json.load(zfile)
+        print("DATA", data.keys())
+        return data
