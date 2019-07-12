0ff97650b9355f87066a2edb508e2caa45f5ecb8
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-12 16:47:37 -0500

    update pshell setup

---
+++ garkbit/scripts/pshell.py
@@ -0,0 +1,31 @@
+
+from contextlib import suppress
+from transaction.interfaces import NoTransaction
+from webtest import TestApp
+
+def setup(env):
+    request = env['request']
+    request.host = 'www.example.com'
+    request.scheme = 'https'
+
+    env['testapp'] = TestApp(env['app'])
+
+    # start a transaction which can be used in the shell
+    request.tm.begin()
+
+    # if using the SQLAlchemy backend from our cookiecutter, the dbsession is
+    # connected to the transaction manager above
+    env['tm'] = request.tm
+    env['dbsession'] = request.dbsession
+    try:
+        yield
+
+    finally:
+        with suppress(NoTransaction):
+            request.tm.abort()
+
+
+def mysetup(env):
+    print("setup called---->{}".format(env))
+    
+          
