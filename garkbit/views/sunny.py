d7b153988c5b0bfa82e8c38df3d7eba54189bfa2
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-12 16:46:55 -0500

    set permissions

--- garkbit/views/sunny.py
+++ garkbit/views/sunny.py
@@ -1,7 +1,7 @@
 import os
 
 from pyramid.security import Allow
-from cornice.resource import resource
+from cornice.resource import resource, view
 # from pyramid.httpexceptions import HTTPNotFound
 
 
@@ -17,7 +17,7 @@ from ..models.sunny import (
     )
 
 
-apiroot = '/api/dev/sunny/{model}'
+apiroot = '/api/dev/sunny/crud/{model}'
 
 Model_Map = dict(geoposition=GeoPosition,
                  maplocation=MapLocation,
@@ -32,15 +32,44 @@ Model_Map = dict(geoposition=GeoPosition,
 
 #@resource(collection_path=apiroot, path=os.path.join(apiroot, '{id}'),
 #          permission='dbadmin')
-@resource(collection_path=apiroot, path=os.path.join(apiroot, '{id}'))
-class ClientView(SimpleModelResource):
+@resource(collection_path=apiroot,
+          path=os.path.join(apiroot, '{id}'),
+          permission='sunny_write')
+class SunnyCrudModelView(SimpleModelResource):
     def __init__(self, request, context=None):
-        super(ClientView, self).__init__(request, context=context)
+        super(SunnyCrudModelView, self).__init__(request, context=context)
         print("MODEL {}", self.model)
 
-    def __acl__bypass(self):
+    def __permitted_methods__(self):
+        return ['collection_get', 'collection_post',
+                'get', 'put']
+
+    def get_model_name(self):
+        return self.request.matchdict['model']
+
+    def serialize_object(self, dbobj):
+        model = self.get_model_name()
+        if model == 'yard':
+            data = dbobj.serialize()
+            data['location'] = dbobj.location.location.serialize()
+            data['client'] = dbobj.client.serialize()
+            return data
+        else:
+            return super(SunnyCrudModelView, self).serialize_object(dbobj)
+            
+    @view(permission='sunny_read')
+    def collection_get(self):
+        return super(SunnyCrudModelView, self).collection_get()
+    
+    def __acl__(self):
         # FIXME use better group principal
-        return [(Allow, 'group:1', 'dbadmin')]
+        return [
+            (Allow, 'group:admin', 'sunny_write'),
+            (Allow, 'group:boss', 'sunny_write'),
+            (Allow, 'group:admin', 'sunny_read'),
+            (Allow, 'group:admin', 'sunny_read'),
+            (Allow, 'group:worker', 'sunny_read'),
+        ]
 
     @property
     def model_map(self):
@@ -48,5 +77,3 @@ class ClientView(SimpleModelResource):
 
     def get_model_type(self):
         return self.request.matchdict['model']
-
-
