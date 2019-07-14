843530308dd2a07a70eabbe0e3f24008c7f6b7c9
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-14 16:33:01 -0500

    work on company views

--- garkbit/views/company.py
+++ garkbit/views/company.py
@@ -27,21 +27,70 @@ from ..models.company import (
     WorkSession,
     )
 
-from ..models.usergroup import User, Group, UserGroup
+# from ..models.usergroup import User, Group, UserGroup
+
+apiroot = '/api/dev/company'
+
+
+boss_root = os.path.join(apiroot, 'boss')
+
+
+@resource(collection_path=boss_root,
+          path=os.path.join(boss_root, '{id}'),
+          permission='boss')
+class BossResource(BaseModelResource):
+    model = Boss
+
+    def __permitted_methods__(self):
+        return ['collection_post',
+                'get', 'put']
+
+    def __acl__(self):
+        acl = [
+            (Allow, 'group:boss', 'boss'),
+            ]
+        return acl
+
+
+company_root = os.path.join(apiroot, 'company')
+
+
+@resource(collection_path=company_root,
+          path=os.path.join(company_root, '{id}'),
+          permission='boss')
+class CompanyResource(BaseModelResource):
+    model = Company
+
+    def __permitted_methods__(self):
+        return ['collection_post', 'collection_get',
+                'get', 'put']
+
+    def __acl__(self):
+        acl = [
+            (Allow, 'group:boss', 'boss'),
+            ]
+        return acl
+
+    def collection_query(self):
+        query = self.db.query(self.model)
+        query = query.filter_by(boss_id=self.request.authenticated_userid)
+        return query
 
 
 Model_Map = dict(boss=Boss, company=Company, worker=Worker,
                  worksession=WorkSession)
-
-apiroot = '/api/dev/company'
 crudroot = os.path.join(apiroot, 'crud/{model}')
+
 
 @resource(collection_path=crudroot,
           path=os.path.join(crudroot, '{id}'),
           permission='admin')
 class CompanyCrudView(SimpleModelResource):
+    model_map = Model_Map
+
     def __permitted_methods__(self):
-        return ['collection_get', 'get']
+        return ['collection_get', 'collection_post',
+                'get']
 
     def __acl__(self):
         acl = [
@@ -59,7 +108,7 @@ class CompanyCrudView(SimpleModelResource):
             data = dict(id=str(dbobj.id), user=user, status=dbobj.status)
             return data
         else:
-            return super(SunnyCrudModelView, self).serialize_object(dbobj)
+            return super(CompanyCrudView, self).serialize_object(dbobj)
         
 
     
