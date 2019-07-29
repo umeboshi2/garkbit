f8209dc65248d2f296676afd2d9b9cf3aeb32e8f
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-07-28 19:05:47 -0500

    start using geocoding queries

--- garkbit/views/places.py
+++ garkbit/views/places.py
@@ -1,4 +1,5 @@
 import os
+from configparser import ConfigParser
 
 from pyramid.security import Allow, Authenticated
 from pyramid.httpexceptions import HTTPForbidden
@@ -7,8 +8,9 @@ from cornice.resource import resource
 # from cornice.resource import view
 
 import transaction
+from querystring_parser import parser as qsparser
 # from sqlalchemy import desc
-# from sqlalchemy import func
+from sqlalchemy import func
 # from sqlalchemy.orm.exc import NoResultFound
 
 from trumpet.views.resourceviews import BaseModelResource
@@ -20,6 +22,12 @@ from ..models.geoposition import (
     )
 
 
+def getboolean(value):
+    if value.lower() not in ConfigParser.BOOLEAN_STATES:
+        raise ValueError('Not a boolean: {}'.format(value))
+    return ConfigParser.BOOLEAN_STATES[value.lower()]
+
+
 apiroot = '/api/dev/places'
 user_root = os.path.join(apiroot, 'user')
 
@@ -29,6 +37,13 @@ user_root = os.path.join(apiroot, 'user')
           permission='auth')
 class UserLocationResource(BaseModelResource):
     model = UserLocation
+
+    def __init__(self, request, context=None):
+        self.request = request
+        self.db = self.request.dbsession
+        self.limit = 25
+        self.max_limit = 100
+        self._use_pagination = False
 
     def __permitted_methods__(self):
         return ['collection_get',
@@ -47,10 +62,26 @@ class UserLocationResource(BaseModelResource):
 
     def collection_query(self):
         query = self.db.query(self.model)
-        groups = [g.name for g in self.request.user.groups]
-        if 'admin' not in groups or True:
+        qs = qsparser.parse(self.request.query_string)
+        all_locs = False
+        if 'all' in qs:
+            all_locs = getboolean(qs['all'])
+        if not all_locs:
             query = query.filter_by(user_id=self.request.user.id)
+        if 'peer' in qs:
+            peer = qs['peer']
+            lat = peer['latitude']
+            long = peer['longitude']
+            radius = peer['radius']
+            geo = "POINT({} {})".format(long, lat)
+            gp_query = self.db.query(GeoPosition.id)
+            f = func.ST_Distance_Sphere(GeoPosition.geo, geo)
+            gp_query = gp_query.filter(f < radius)
+            query = query.filter(UserLocation.id.in_(gp_query))
         return query
+
+    def collection_get(self):
+        return super(UserLocationResource, self).collection_get()
 
     def collection_post(self):
         data = self.request.json
@@ -86,6 +117,32 @@ class UserLocationResource(BaseModelResource):
             self.db.delete(geoposition)
             self.db.flush()
         return dict(result='success')
-    
+
+
+# func.ST_Distance_Sphere(City.geo, self.geo)
+peer_root = os.path.join(apiroot, 'peers', '{id}')
+
+
+@resource(collection_path=peer_root,
+          path=os.path.join(peer_root, '{content}'),
+          permission='auth')
+class UserPeerResource(BaseModelResource):
+    model = UserLocation
+
+    def collection_query(self):
+        query = self.db.query(self.model)
+        id = self.request.matchdict['id']
+        with transaction.manager:
+            location = query.get(id)
+            geo = location.geo
+            if location.geo is None:
+                raise RuntimeError("No geo info for {}".format(id))
             
-                
+        qs = qsparser.parse(self.request.query_string)
+        all_locs = False
+        if 'all' in qs:
+            all_locs = qs['all']
+            print("all_locs", all_locs)
+        if not all_locs:
+            query = query.filter_by(user_id=self.request.user.id)
+        return query
