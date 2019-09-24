e2c0c7e819586c453d87e337de6c103229926490
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-09-24 18:09:10 -0500

    add yard session api

--- garkbit/views/sunny.py
+++ garkbit/views/sunny.py
@@ -1,18 +1,20 @@
 import os
 
 from pyramid.security import Allow
+from pyramid.httpexceptions import HTTPNotAcceptable
 from cornice.resource import resource, view
-# from pyramid.httpexceptions import HTTPNotFound
-
+from sqlalchemy.orm.exc import NoResultFound
+import transaction
 
 from trumpet.views.resourceviews import SimpleModelResource
-
+from trumpet.views.resourceviews import BaseModelResource
 from ..models.geoposition import GeoPosition
 from ..models.sunny import (
     SunnyClient,
     Yard,
     SingleClientJob,
     SingleYardJob,
+    YardSession,
     )
 
 
@@ -37,7 +39,6 @@ class SunnyCrudModelView(SimpleModelResource):
     def __permitted_methods__(self):
         return ['collection_get', 'collection_post',
                 'get', 'put']
-
 
     def serialize_object(self, dbobj):
         model = self.get_model_name()
@@ -73,3 +74,78 @@ class SunnyCrudModelView(SimpleModelResource):
 
     def get_model_name(self):
         return self.request.matchdict['model']
+
+
+##################################################
+# yard clock view
+##################################################
+clock_root = os.path.join(apiroot, 'yard-clock', '{yard_id}')
+
+
+@resource(collection_path=clock_root,
+          path=os.path.join(clock_root, '{id}'),
+          permission='punch')
+class YardClockView(BaseModelResource):
+    def __acl__(self):
+        acl = [
+            (Allow, 'group:worker', 'punch'),
+            ]
+        return acl
+
+    @view(permission='punch')
+    def collection_post(self):
+        """Worker clocks in."""
+        yard_id = self.request.matchdict['yard_id']
+        worker = self.db.query(Worker).get(self.request.user.id)
+        latest = self._get_latest_session()
+        if latest.status == 'on':
+            raise HTTPNotAcceptable
+        with transaction.manager:
+            session = YardSession()
+            session.yard_id = yard_id
+            session.worker_id = worker.id
+            session.status = 'on'
+            self.db.add(session)
+
+    def _get_latest_session(self):
+        yard_id = self.request.matchdict['yard_id']
+        q = self.db.query(YardSession).filter_by(yard_id=yard_id)
+        q = q.order_by(desc('start')).limit(1)
+        return q.one()
+
+    @view(permission='punch')
+    def put(self):
+        """Worker clocks out."""
+        yard_id = self.request.matchdict['yard_id']
+        worker = self.db.query(Worker).get(self.request.user.id)
+        # get latest work session
+        session = self._get_latest_session(worker.id)
+        if session.status == 'off':
+            raise HTTPNotAcceptable
+        # assert that latest session is same as
+        # requested session
+        if str(session.id) != self.request.matchdict['id']:
+            raise HTTPNotAcceptable
+        with transaction.manager:
+            session.end = func.now()
+            session.status = 'off'
+            self.db.add(session)
+
+    @view(permission='punch')
+    def collection_get(self):
+        yard_id = self.request.matchdict['yard_id']
+        try:
+            session = self._get_latest_session()
+        except NoResultFound:
+            session = None
+            id = None
+        # include session id as the id so that we can PUT when
+        # clocking out
+        if session is not None:
+            id = str(session.id)
+            session = session.serialize()
+        data = dict(session=session, id=id)
+        return data
+
+
+    
